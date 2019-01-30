1df28991600f8dc9ecfbf1c0aa00e09611514a4a
emmanio <emmanioashley@gmail.com>
2019-01-30 21:39:57 +1300

    removed import, file was empty

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
 
-import './app.css'
 import Home from '../Home'
 import Login from '../Login/Login'
 import Register from '../Register/Register'
@@ -10,14 +9,14 @@ import Profile from '../Profile'
 import Addproduct from '../Product/Addproduct'
 
 export default class App extends Component {
-  render () {
+  render() {
     return (
       <div className="app pure-g">
         <Route exact path='/' component={Home} />
         <Route path='/login' component={Login} />
         <Route path='/register' component={Register} />
         <Route path='/neighbourhood' component={Area} />
-        <Route path='/addproducts' component={Addproduct}/>
+        <Route path='/addproducts' component={Addproduct} />
         <Route path='/profile' component={Profile} />
       </div>
     )
