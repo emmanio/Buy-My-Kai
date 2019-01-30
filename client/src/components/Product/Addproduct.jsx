7da0802cf3812b16567e870f321b3d9e4cc539ad
emmanio <emmanioashley@gmail.com>
2019-01-30 21:49:45 +1300

    refactored code, removed constructor and bound events

--- client/src/components/Product/Addproduct.jsx
+++ client/src/components/Product/Addproduct.jsx
@@ -1,46 +1,39 @@
 import React from 'react'
 // import {Redirect} from 'react-router-dom'
-import {connect} from 'react-redux'
-import {sendProduct} from '../../actions/products'
-import {Button} from '@material-ui/core'
+import { connect } from 'react-redux'
+import { sendProduct } from '../../actions/products'
+import { Button } from '@material-ui/core'
 
 export class Addproduct extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      productName: '',
-      price: '',
-      quantity: '',
-      productDescription: '',
-      organic: false,
-      freerange: false,
-      productSubmitted: false
-    }
-    this.handleChange = this.handleChange.bind(this)
-    this.handleOrganic = this.handleOrganic.bind(this)
-    this.handleFreerange = this.handleFreerange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
+  state = {
+    productName: '',
+    price: '',
+    quantity: '',
+    productDescription: '',
+    organic: false,
+    freerange: false,
+    productSubmitted: false
   }
 
-  handleChange (e) {
+  handleChange(e) {
     this.setState({
       [e.target.name]: e.target.value
     })
   }
 
-  handleOrganic (e) {
+  handleOrganic() {
     this.setState({
       organic: !this.state.organic
     })
   }
 
-  handleFreerange (e) {
+  handleFreerange() {
     this.setState({
       freerange: !this.state.freerange
     })
   }
 
-  handleSubmit () {
+  handleSubmit() {
     this.props.dispatch(sendProduct(this.state))
     this.setState({
       productSubmitted: !this.state.productSubmitted,
@@ -51,7 +44,8 @@ export class Addproduct extends React.Component {
     })
   }
 
-  render () {
+  render() {
+    const state = state
     return (
       <div className='container pure-u-1'>
         <div className="pure-form pure-u-1">
@@ -60,34 +54,34 @@ export class Addproduct extends React.Component {
               type="text"
               className="pure-input-rounded"
               name='productName'
-              value={this.state.productName}
+              value={state.productName}
               placeholder='Product name'
-              onChange={this.handleChange}></input>
+              onChange={() => this.handleChange}></input>
           </div>
           <div className='price'>
             <input
               type="text"
               className="pure-input-rounded"
               name='price'
-              value={this.state.price}
+              value={state.price}
               placeholder='Price'
-              onChange={this.handleChange}></input>
+              onChange={() => this.handleChange}></input>
           </div>
           <div className='price'>
             <input
               name='quantity'
               className="pure-input-rounded"
-              value={this.state.quantity}
+              value={state.quantity}
               placeholder='Quantity'
-              onChange={this.handleChange}></input>
+              onChange={() => this.handleChange}></input>
           </div>
           <div className='price'>
             <textarea
               name='productDescription'
               className="pure-input-rounded"
-              value={this.state.productDescription}
+              value={state.productDescription}
               placeholder='Description'
-              onChange={this.handleChange}/>
+              onChange={() => this.handleChange} />
           </div>
           <div className="organic">
             <h5>Organic</h5>
@@ -95,13 +89,13 @@ export class Addproduct extends React.Component {
               className="checkbox"
               type='checkbox'
               name='organic'
-              value={this.state.organic}
-              onClick={this.handleOrganic}></input>
+              value={state.organic}
+              onClick={() => this.handleOrganic}></input>
           </div>
         </div>
 
         <Button className='btn btn-third' onClick={this.handleSubmit}>Done</Button>
-        <div>{this.state.productSubmitted ? <h5>Successfully added</h5> : <div></div>}</div>
+        <div>{state.productSubmitted ? <h5>Successfully added</h5> : <div></div>}</div>
       </div>
     )
   }
